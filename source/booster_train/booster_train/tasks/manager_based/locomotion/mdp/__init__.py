d599b2a5bc6de5ad9c66464062e0292313e5aa36
norikenpi <norikenpi@gmail.com>
2026-05-24 23:20:58 +0900

    feat: add K1 locomotion training task

---
+++ source/booster_train/booster_train/tasks/manager_based/locomotion/mdp/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""MDP terms for Booster locomotion tasks."""
+
+from isaaclab_tasks.manager_based.locomotion.velocity.mdp import *  # noqa: F401, F403
+
+from .observations import *  # noqa: F401, F403
+from .rewards import *  # noqa: F401, F403
